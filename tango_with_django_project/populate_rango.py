c311c9f0539708c882666171d6f86358eea5b3b3
Roman Ch <hello@romangagarin.com>
2018-03-27 23:08:25 +0100

    add tests

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -32,9 +32,9 @@ def populate():
          "url":"http://bottlepy.org/docs/dev/"},
         {"title":"Flask",
          "url":"http://flask.pocoo.org"} ]
-    cats = {"Python": {"pages": python_pages},
-            "Django": {"pages": django_pages},
-            "Other Frameworks": {"pages": other_pages} }
+    cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
+            "Django": {"pages": django_pages, "views": 64, "likes": 32},
+            "Other Frameworks": {"pages": other_pages, "views": 64, "likes": 32} }
     # If you want to add more categories or pages,
     # add them to the dictionaries above.
     # The code below goes through the cats dictionary, then adds each category,
@@ -44,7 +44,7 @@ def populate():
     # for more information about how to iterate over a dictionary properly.
 
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data["pages"]:
             add_page(c, p["title"], p["url"])
 
@@ -62,8 +62,10 @@ def add_page(cat, title, url, views=0):
     return p
 
 
-def add_cat(name):
+def add_cat(name, views, likes):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
     c.save()
     return c
 
--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -3,11 +3,13 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
 
     class Meta:
         verbose_name_plural = 'Categories'
 
-    def __str__(self): # For Python 2, use __unicode__ too
+    def __str__(self):
         return self.name
 
 
@@ -17,5 +19,5 @@ class Page(models.Model):
     url = models.URLField()
     views = models.IntegerField(default=0)
 
-    def __str__(self): # For Python 2, use __unicode__ too
+    def __str__(self):
         return self.title
